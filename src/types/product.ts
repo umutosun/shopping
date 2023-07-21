16d5f6d7cb698fce3570a97a48f6a3f29c8f6d02
Umut Tosun <umut35137@gmail.com>
2023-07-21 10:12:32 +0300

    Feat: type designations

---
+++ src/types/product.ts
@@ -0,0 +1,28 @@
+export interface Product {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  rating: {
+    count: number;
+    rate: number;
+  };
+  title: string;
+  quantity: number;
+}
+export interface ProductState {
+  data: Product[];
+  basket: Product[];
+  loading: boolean;
+  error: string;
+  detail: Product[];
+}
+export interface Category {
+  category: [];
+}
+export interface BasketState {
+  products: Product[];
+  itemCount: number;
+  totalAmount: number;
+}
